98d9d6258969882dc5259cf31514f9d6ffe84047
Yurii <yurii@Yuriis-MacBook-Pro.local>
2026-04-16 18:35:55 +0200

    added jwt

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,16 +1,22 @@
 import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import type { GoogleUser, RequestWithUser } from 'src/interface.type';
+import type { RequestWithUser } from 'src/interface.type';
+import { AuthService } from './auth.service';
+import { User } from 'src/users/user.entity';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private authService: AuthService) {}
   @Get('google')
   @UseGuards(AuthGuard('google'))
   async googleAuth() {}
 
   @Get('google/redirect')
   @UseGuards(AuthGuard('google'))
-  googleAuthRedirect(@Req() req: RequestWithUser): GoogleUser {
-    return req.user;
+  googleAuthRedirect(@Req() req: RequestWithUser): Promise<{
+    user: User | null;
+    token: string;
+  }> {
+    return this.authService.login(req.user.email);
   }
 }
