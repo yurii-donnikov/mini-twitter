3f8cfa4d427e3858b9ce1fd2d0d1518390b1854a
Yurii <yurii@Yuriis-MacBook-Pro.local>
2026-04-29 20:48:51 +0200

    added routes login and profile

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { authRoutes } from './auth/auth.routes';
+import { profileRoutes } from './profile/profile.routes';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  ...authRoutes,
+  ...profileRoutes,
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+];
---
+++ frontend/src/app/core/guards/guest.guard.ts
@@ -0,0 +1,15 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+
+export const guestGuard: CanActivateFn = () => {
+  const router = inject(Router);
+  const token = localStorage.getItem('token');
+
+  //   if (token) {
+  //     router.navigate(['/home']);
+  //     return false;
+  //   }
+
+  //   return true;
+  return true;
+};
